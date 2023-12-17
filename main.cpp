9e6bfb7953b166015da2220bce828fafb27036a6
Facundo <facuguialtana7@gmail.com>
2023-12-17 12:41:29 -0300

    Getting files setted3

---
+++ main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+void mainMenu(){
+  while (true){
+    cout << "> ";
+    string action;
+    getline(cin, action);
+    cout << action << endl;
+  }
+}
+
+
+int main(int argc, char const *argv[]){
+  cout << argv[0] << ' ' << argv[1] << endl;
+  mainMenu(); 
+  return 0;
+}
